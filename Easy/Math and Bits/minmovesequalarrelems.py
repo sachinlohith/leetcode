42afd1fcff7f9788878b4261c097a6dd85768311
Sachin Lohith <sachinlohith93@gmail.com>
2017-07-08 00:00:49 -0700

    Added soln explanation to a math problem

--- Easy/Math and Bits/minmovesequalarrelems.py
+++ Easy/Math and Bits/minmovesequalarrelems.py
@@ -14,6 +14,22 @@ Explanation:
 Only three moves are needed (remember each move increments two elements):
 
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+
+Soln:
+Let's define sum as the sum of all the numbers, before any moves;
+minNum as the min number int the list; n is the length of the list;
+
+After, say m moves, we get all the numbers as x ,
+and we will get the following equation
+
+ sum + m * (n - 1) = x * n
+and actually,
+
+  x = minNum + m
+and finally, we will get
+
+  sum - minNum * n = m
+So, it is clear and easy now.
 """
 
 
